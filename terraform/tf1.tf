51c6478842e273d5d1336c120390a8d2eb9dd622
nannaka nageswararao <nannaka.nageswararao1999@gmail.com>
2024-12-23 20:26:25 -0600

    terraform added new files and created ec2 instance

--- terraform/tf1.tf
+++ terraform/tf1.tf
@@ -1,13 +1,16 @@
 provider "aws" {
-  region = us-east-1
+  region = "us-east-1"
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-0b4624933067d393a" # Amazon Linux 2 AMI
-  instance_type = t2.small
-  key_pair  = "key1.pem"
+  ami           = "ami-01816d07b1128cd2d" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = "key1"  # Correct attribute for key pair
+  subnet_id     = "subnet-0c5e504645e99e38e"
+  monitoring    = true
 
   tags = {
     Name = "JenkinsServer"
   }
 }
+
